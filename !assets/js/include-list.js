e68ee36168fc248cb6b5ae6448a01013f816a890
Jon Erik Solheim <jones@restdb.io>
2016-06-22 12:44:09 +0200

    Initial upload
    Static files for web theme

---
+++ !assets/js/include-list.js
@@ -0,0 +1,7 @@
+
+$(function(){
+  $("#includedSidebar").load("../!assets/common/sidebar.html");
+  $("#includedHeaderCore").load("../!assets/common/header-core.html");
+  $("#includedFooterMap").load("../!assets/common/footer-map.html");
+  $("#includedFooterCore").load("../!assets/common/footer-core.html");
+});
